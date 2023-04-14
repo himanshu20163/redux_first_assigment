5ddc7a9f92f980e06e40351db5d21cbb946e8d12
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-14 10:29:40 -0700

    Delete Redux directory

--- Redux/Store.jsx
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore,applyMiddleware } from "redux";
-
-import myreducer from "./Reducer";
-import logger from "redux-logger";
-
-const mystore = legacy_createStore(myreducer,applyMiddleware(logger));
-export default mystore;
